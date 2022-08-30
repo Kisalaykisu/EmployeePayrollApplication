34f7a15c95354f2bb35f02a10ae99348eaf443b9
Kisalay <kisalaykisu@gmail.com>
2022-08-30 11:01:03 +0530

    UC2 has been added:

---
+++ src/main/java/com/example/employeepayrollapplication/controller/EmployeeController.java
@@ -0,0 +1,59 @@
+package com.example.employeepayrollapplication.controller;
+
+import com.example.employeepayrollapplication.model.EmployeeEntity;
+import com.example.employeepayrollapplication.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+        @Autowired
+        EmployeeService service;
+
+        @RequestMapping(value = {"", "/", "/home"}, method = RequestMethod.GET)
+        public String greet() {
+                return "Hello, Welcome to Employee Payroll App";
+        }
+        /*
+        Extending GreetingController to use Services Layer to get Simple Greeting message
+         */
+
+        @GetMapping("/service")
+        public String serviceCall() {
+                return service.welcomeMessage();
+        }
+        //Insert Employee Data
+        @PostMapping("/post")
+        public EmployeeEntity addEmpData(@RequestBody EmployeeEntity empData) {
+                service.saveData(empData);
+                return empData;
+        }
+
+        //Get by ID
+        @GetMapping("/id/{id}")
+        public Optional<EmployeeEntity> greetById(@PathVariable Long id){
+                return service.findById(id);
+        }
+
+        //get all the data
+        @GetMapping("/all")
+        public List<EmployeeEntity> findAllData() {
+                return service.findAllData();
+        }
+        //Edit or Update the data by id
+        @PutMapping("/edit/{id}")
+        public EmployeeEntity editGreeting(@RequestBody EmployeeEntity empData, @PathVariable long id) {
+                return service.editData(empData, id);
+        }
+
+        //Delete the data by id
+        @DeleteMapping("/delete/{id}")
+        public String deleteGreeting(@PathVariable Long id) {
+                service.deleteData(id);
+                return "Employee Data deleted";
+        }
+}
---
+++ src/main/java/com/example/employeepayrollapplication/service/EmployeeService.java
@@ -0,0 +1,52 @@
+package com.example.employeepayrollapplication.service;
+
+import com.example.employeepayrollapplication.model.EmployeeEntity;
+import com.example.employeepayrollapplication.repo.Repo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    Repo repository;
+    public String welcomeMessage(){
+        return "This is Employee Payroll App";
+    }
+
+    public EmployeeEntity saveData(EmployeeEntity empData) {
+        repository.save(empData);
+        return empData;
+    }
+
+    public Optional<EmployeeEntity> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<EmployeeEntity> findAllData() {
+        return repository.findAll();
+    }
+
+    public EmployeeEntity editData(EmployeeEntity empData, Long id) {
+        EmployeeEntity existingGreet = repository.findById(id).orElse(null);
+        if (existingGreet != null) {
+            existingGreet.setEmployeeId(empData.getEmployeeId());
+            existingGreet.setEmployeeName(empData.getEmployeeName());
+            existingGreet.setProfilePic(empData.getProfilePic());
+            existingGreet.setGender(empData.getGender());
+            existingGreet.setSalary(empData.getSalary());
+            existingGreet.setDepartment(empData.getDepartment());
+            existingGreet.setStartDate(empData.getStartDate());
+            existingGreet.setNotes(empData.getNotes());
+            return repository.save(existingGreet);
+        }
+        else
+            return null;
+    }
+
+    public void deleteData(Long id) {
+        repository.deleteById(id);
+    }
+}
